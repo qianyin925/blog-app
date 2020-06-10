ca7d375efbe0c8aafb9a83d63841b32270abbda7
qianyin925 <qianyin95@gmail.com>
2020-06-10 14:36:20 +0800

    build: react-navigation 依赖安装配置

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import 'react-native-gesture-handler';
 
-export default () => (
-  <View>
-    <Text>name</Text>
-  </View>
-);
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
