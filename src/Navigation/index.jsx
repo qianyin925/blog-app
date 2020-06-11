cf95eccc3169674ad770c5ead31275a50c53a95e
qianyin925 <qianyin95@gmail.com>
2020-06-11 14:44:08 +0800

    build: 路由配置

--- src/Navigation/index.jsx
+++ src/Navigation/index.jsx
@@ -1,38 +1,23 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import setting from './setting';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
-
-const style = {
-  flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
-};
-
-const HomeScreen = () => (
-  <View style={style}>
-    <Text>Home Screen</Text>
-  </View>
-);
-
-const DetailsScreen = () => (
-  <View>
-    <Text>details Screen</Text>
-  </View>
-);
 
 const Stack = createStackNavigator();
 
 export default () => (
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen
-        name="Details"
-        component={DetailsScreen}
-        options={{ title: 'Overview' }}
-      />
-      <Stack.Screen name="Home" component={HomeScreen} />
+      {setting.map((v, index) => (
+        <Stack.Screen
+          key={index}
+          name={v.name}
+          options={v.options}
+          component={v.component}
+        />
+      ))}
     </Stack.Navigator>
   </NavigationContainer>
 );
---
+++ src/Navigation/setting.js
@@ -0,0 +1,15 @@
+import Home from '../screens/Home';
+import Detail from '../screens/Detail';
+
+export default [
+  {
+    name: 'Home',
+    component: Home,
+    options: {},
+  },
+  {
+    name: 'Detail',
+    component: Detail,
+    options: { title: 'Overview' },
+  },
+];
